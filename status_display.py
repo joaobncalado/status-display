9e35da3fb2be054db7b3c93907ecd2d304093c47
João Calado <joao.calado@inm.pt>
2025-05-08 14:35:23 +0100

    updated info displayed, updated font to improve readability, improved logs

--- status_display.py
+++ status_display.py
@@ -16,28 +16,27 @@ PW_PATH = '/home/secrets/pihole_pw'
 LOW_BATTERY_THRESHOLD = 20
 
 # === Aux funcs ===
-def read_from_file(path):
+def readFromFile(path):
     try:
         with open(path, 'r') as f:
             return f.read().strip()
     except Exception as e:
-        LOG.error(f"Error loading file content: {e}")
+        LOG.error(f"readFromFile - Error loading file content: {e}")
         return None
 
-def get_battery():
+def getBattery():
     try:
-        LOG.debug("Getting battery info via netcat")
+        LOG.debug("getBattery - Getting battery info via netcat")
         command = 'echo "get battery" | nc -q 0 127.0.0.1 8423'
         output = subprocess.check_output(command, shell=True, text=True)
         for line in output.splitlines():
             if line.lower().startswith("battery:"):
                 battery_value = float(line.split(":")[1].strip())
-                LOG.debug(f"Battery % is: {battery_value}")
+                LOG.debug(f"getBattery - Battery % is: {battery_value}")
                 return int(round(battery_value))
-        LOG.warning("Battery info not found in output")
         return "N/A"
     except Exception as e:
-        LOG.error(f"Error getting battery status via netcat: {e}")
+        LOG.error(f"getBattery - Error getting battery status via netcat: {e}")
         return "N/A"
 
 # === PiHole funcs ===
@@ -51,7 +50,7 @@ def authenticate(ip, password):
             raise Exception("Session is not valid")
         return data.get("session", {}).get("sid")
     except Exception as e:
-        LOG.error(f"Authentication failed for {ip}: {e}")
+        LOG.error(f"authenticate - Authentication failed for {ip}: {e}")
         return None
 
 def logout(ip, sid):
@@ -59,131 +58,140 @@ def logout(ip, sid):
     try:
         requests.delete(url, headers={"sid": sid}, timeout=5)
     except Exception as e:
-        LOG.error(f"Logout failed for {ip}: {e}")
+        LOG.error(f"logout - Logout failed for {ip}: {e}")
 
-def get_pihole_data(ip, password):
+def getPiHoleData(ip, password):
     sid = authenticate(ip, password)
     if not sid:
-        return "Error", 0.0
+        return "N/A", 0.0
     try:
-        status_url = f"http://{ip}/api/dns/blocking"
-        response = requests.get(status_url, headers={"sid": sid}, timeout=5)
-        status = response.json().get("blocking", "N/A")
         summary_url = f"http://{ip}/api/stats/summary"
         response = requests.get(summary_url, headers={"sid": sid}, timeout=5)
-        blocked_percentage = response.json()["queries"].get("percent_blocked", 0.0)
-        return status, float(f"{blocked_percentage:.2f}")
+        resquestsNumber = response.json()["queries"].get("total")
+        blockedPercentage = response.json()["queries"].get("percent_blocked", 0.0)
+        return resquestsNumber, float(f"{blockedPercentage:.2f}")
     except Exception as e:
-        LOG.error(f"Error getting PiHole data from {ip}: {e}")
+        LOG.error(f"getPiHoleData - Error getting PiHole data from {ip}: {e}")
         return "N/A", 0.0
     finally:
         logout(ip, sid)
 
 # === System ===
-def get_local_uptime():
+def getLocalUpTime():
     try:
         with open("/proc/uptime", "r") as f:
-            uptime_seconds = float(f.readline().split()[0])
-            hours = int(uptime_seconds // 3600)
-            minutes = int((uptime_seconds % 3600) // 60)
+            uptimeSeconds = float(f.readline().split()[0])
+            hours = int(uptimeSeconds // 3600)
+            minutes = int((uptimeSeconds % 3600) // 60)
             return f"{hours}h {minutes}m"
     except Exception as e:
-        LOG.error(f"Error reading local uptime: {e}")
+        LOG.error(f"getLocalUpTime - Error reading local uptime: {e}")
         return "N/A"
 
-def get_local_cpu_temp():
+def getLocalCPUTemp():
     try:
         with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
             return round(int(f.read()) / 1000, 1)
     except Exception as e:
-        LOG.error(f"Error reading local CPU temp: {e}")
+        LOG.error(f"getLocalCPUTemp - Error reading local CPU temp: {e}")
         return "N/A"
 
-def get_remote_uptime(ip, user, password):
+def getRemoteUpTime(ip, user, password):
     try:
-        LOG.debug(f"Reading remote uptime on: {ip}")
+        LOG.debug(f"getRemoteUpTime - Reading remote uptime on: {ip}")
         command = f"sshpass -p '{password}' ssh -o StrictHostKeyChecking=no {user}@{ip} cat /proc/uptime"
         output = subprocess.getoutput(command)
-        uptime_seconds = float(output.split()[0])
-        hours = int(uptime_seconds // 3600)
-        minutes = int((uptime_seconds % 3600) // 60)
+        uptimeSeconds = float(output.split()[0])
+        hours = int(uptimeSeconds // 3600)
+        minutes = int((uptimeSeconds % 3600) // 60)
         return f"{hours}h {minutes}m"
     except Exception as e:
-        LOG.error(f"Error reading remote uptime: {e}")
+        LOG.error(f"getRemoteUpTime - Error reading remote uptime: {e}")
         return "N/A"
 
-def get_remote_cpu_temp(ip, user, password):
+def getRemoteCPUTemp(ip, user, password):
     try:
-        LOG.debug(f"Reading remote CPU Temp on: {ip}")
+        LOG.debug(f"getRemoteCPUTemp - Reading remote CPU Temp on: {ip}")
         command = f"sshpass -p '{password}' ssh -o StrictHostKeyChecking=no {user}@{ip} cat /sys/class/thermal/thermal_zone0/temp"
         output = subprocess.getoutput(command)
         return round(int(output.strip()) / 1000, 1)
     except Exception as e:
-        LOG.error(f"Error reading remote CPU temp: {e}")
+        LOG.error(f"getRemoteCPUTemp - Error reading remote CPU temp: {e}")
         return "N/A"
 
 # === Main ===
 def main():
-    LOG.debug("Starting execution")
-    pihole1_ip = '192.168.50.135'
-    pihole2_ip = '127.0.0.1'
+    LOG.debug("main - Starting execution")
+    pihole1Ip = '192.168.50.135'
+    pihole2Ip = '127.0.0.1'
 
-    user = read_from_file(USER_PATH)
-    password = read_from_file(PW_PATH)
+    user = readFromFile(USER_PATH)
+    password = readFromFile(PW_PATH)
 
-    pihole1_status, pihole1_blocked = get_pihole_data(pihole1_ip, password)
-    pihole2_status, pihole2_blocked = get_pihole_data(pihole2_ip, password)
+    pihole1RequestsNumber, pihole1Blocked = getPiHoleData(pihole1Ip, password)
+    pihole2RequestsNumber, pihole2Blocked = getPiHoleData(pihole2Ip, password)
 
-    pihole1_uptime = get_remote_uptime(pihole1_ip, user, password)
-    pihole1_temp = get_remote_cpu_temp(pihole1_ip, user, password)
+    pihole1UpTime = getRemoteUpTime(pihole1Ip, user, password)
+    pihole1Temp = getRemoteCPUTemp(pihole1Ip, user, password)
 
-    pihole2_uptime = get_local_uptime()
-    pihole2_temp = get_local_cpu_temp()
+    pihole2UpTime = getLocalUpTime()
+    pihole2Temp = getLocalCPUTemp()
 
-    battery = get_battery()
-
-    epd = epd2in13_V2.EPD()
-    epd.init(epd.FULL_UPDATE)
-    width, height = epd.height, epd.width
-
-    image = Image.new('1', (width, height), 255)
-    draw = ImageDraw.Draw(image)
-
-    font_small = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 14)
-    font_small_bold = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 14)
+    battery = getBattery()
 
     now_str = datetime.datetime.now().strftime("%d/%m %H:%M")
 
-    draw.text((1, 6), f"PiHole .135", font=font_small_bold, fill=0)
-    draw.text((133, 6), f"PiHole .136", font=font_small_bold, fill=0)
+    epd = epd2in13_V2.EPD()
+    epd.init(epd.PART_UPDATE)
+    width, height = epd.height, epd.width
+    image = Image.new('1', (width, height), 255)
+    draw = ImageDraw.Draw(image)
 
-    draw.text((1, 25), f"Status: {pihole1_status}", font=font_small, fill=0)
-    draw.text((1, 44), f"Up: {pihole1_uptime}", font=font_small, fill=0)
-    draw.text((1, 63), f"Blocked: {pihole1_blocked}%", font=font_small, fill=0)
-    draw.text((1, 82), f"Temp: {pihole1_temp}°C", font=font_small, fill=0)
+    fontSmall = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 14)
+    fontSmallBold = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 14)
+    fontBigBold = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 16)
 
-    draw.text((133, 25), f"Status: {pihole2_status}", font=font_small, fill=0)
-    draw.text((133, 44), f"Up: {pihole2_uptime}", font=font_small, fill=0)
-    draw.text((133, 63), f"Blocked: {pihole2_blocked}%", font=font_small, fill=0)
-    draw.text((133, 82), f"Temp: {pihole2_temp}°C", font=font_small, fill=0)
+    draw.text((10, 4), "PiHole .135", font=fontBigBold, fill=0)
+    draw.text((135, 4), "PiHole .136", font=fontBigBold, fill=0)
+
+    draw.text((10, 25), "REQ:", font=fontSmallBold, fill=0)
+    draw.text((50, 25), f"{pihole1RequestsNumber}", font=fontSmall, fill=0)
+    draw.text((10, 44), "BLKD:", font=fontSmallBold, fill=0)
+    draw.text((60, 44), f"{pihole1Blocked}%", font=fontSmall, fill=0)
+    draw.text((10, 63), "UP:", font=fontSmallBold, fill=0)
+    draw.text((40, 63), f"{pihole1UpTime}", font=fontSmall, fill=0)
+    draw.text((10, 82), "TEMP:", font=fontSmallBold, fill=0)
+    draw.text((60, 82), f"{pihole1Temp}°C", font=fontSmall, fill=0)
+
+    draw.text((135, 25), "REQ:", font=fontSmallBold, fill=0)
+    draw.text((175, 25), f"{pihole2RequestsNumber}", font=fontSmall, fill=0)
+    draw.text((135, 44), "BLKD:", font=fontSmallBold, fill=0)
+    draw.text((185, 44), f"{pihole2Blocked}%", font=fontSmall, fill=0)
+    draw.text((135, 63), "UP:", font=fontSmallBold, fill=0)
+    draw.text((165, 63), f"{pihole2UpTime}", font=fontSmall, fill=0)
+    draw.text((135, 82), "TEMP:", font=fontSmallBold, fill=0)
+    draw.text((185, 82), f"{pihole2Temp}°C", font=fontSmall, fill=0)
 
     # Separator lines
-    draw.line((123, 0, 123, 100), fill=0)  # Vertical separator
+    draw.line((127.5, 0, 127.5, 100), fill=0)  # Vertical separator
     draw.line((0, 101, width, 101), fill=0)   # Horizontal footer separator
 
+    # Battery
     if isinstance(battery, int):
-        battery_bar_length = int(50 * battery / 100)
-        draw.rectangle((1, 107, 10 + battery_bar_length, 117), fill=0)
-        draw.rectangle((1, 107, 60, 117), outline=0)
-        draw.text((75, 105), f"{round(battery)}%", font=font_small, fill=0)
+        batteryBarLength = int(60 * battery / 100)
+        draw.text((85, 105), f"{round(battery)}%", font=fontSmall, fill=0)
     else:
-        draw.text((1, 105), "Battery: N/A", font=font_small, fill=0)
+        batteryBarLength = 0
+        draw.text((85, 105), "N/A", font=fontSmall, fill=0)
+    draw.rectangle((10, 107, 10 + batteryBarLength, 117), fill=0)
+    draw.rectangle((10, 107, 70, 117), outline=0)
 
-    draw.text((160, 105), now_str, font=font_small, fill=0)
+    # Date-time
+    draw.text((155, 105), now_str, font=fontSmall, fill=0)
 
-    rotated_image = image.rotate(180)
-    color_inverted_image = ImageOps.invert(rotated_image.convert("L")).convert("1")
-    epd.display(epd.getbuffer(rotated_image))
+    rotatedImage = image.rotate(180)
+    colorInvertedImage = ImageOps.invert(rotatedImage.convert("L")).convert("1")
+    epd.display(epd.getbuffer(colorInvertedImage))
     epd.sleep()
 
 if __name__ == "__main__":
